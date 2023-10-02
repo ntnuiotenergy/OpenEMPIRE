91349c0010fc268937c3519992bbaba0da896bb8
= <martin.hjelmeland@ntnu.no>
2023-10-02 14:49:09 +0200

    Structure configuration into classes

---
+++ Empire/config.py
@@ -0,0 +1,144 @@
+from pathlib import Path
+from typing import Dict
+from datetime import datetime
+import yaml
+
+
+def read_config_file(path: Path) -> Dict:
+    with open(path) as file:
+        config = yaml.safe_load(file)
+
+    return config
+
+
+class EmpireConfiguration:
+    def __init__(
+            self, 
+            use_temporary_directory: bool,
+            temporary_directory: str | Path,
+            forecast_horizon_year: int,
+            number_of_scenarios: int,
+            length_of_regular_season: int,
+            discount_rate: float,
+            wacc: float,
+            optimization_solver: str,
+            use_scenario_generation: bool,
+            use_fixed_sample: bool,
+            use_emission_cap: bool,
+            print_in_iamc_format: bool,
+            write_in_lp_format: bool,
+            serialize_instance: bool,
+            north_sea: bool,
+        ):
+        """
+        Class containing configurations for running Empire simulations.
+
+        :param use_temporary_directory: Specifies whether to use a temporary directory for operations
+        :param temporary_directory: Path to the temporary directory used for certain operations
+        :param forecast_horizon_year: Target or end year for a simulation or forecast
+        :param number_of_scenarios: Number of scenarios to consider in the simulation or modeling
+        :param length_of_regular_season: Length of a regulatory or regular season (e.g., in hours)
+        :param discount_rate: Rate used to discount future cash flows to present value
+        :param wacc: Weighted Average Cost of Capital (WACC)
+        :param optimization_solver: Mathematical solver used for optimization tasks
+        :param use_scenario_generation: Indicates if scenarios should be generated dynamically
+        :param use_fixed_sample: Specifies if a certain sample or set of data is fixed
+        :param use_emission_cap: Indicates if there's a cap on emissions
+        :param print_in_iamc_format: Output should be printed in a specific format (e.g., IAMC-compatible)
+        :param write_in_lp_format: Problem should be written in Linear Programming format
+        :param serialize_instance: Serialize the data structure or model for later use
+        :param use_north_sea: Whether north-sea is modelled or not
+        """
+        # Model parameters
+        self.use_temporary_directory = use_temporary_directory
+        self.temporary_directory = Path(temporary_directory)
+        self.forecast_horizon_year = forecast_horizon_year
+        self.number_of_scenarios = number_of_scenarios
+        self.length_of_regular_season = length_of_regular_season
+        self.discount_rate = discount_rate
+        self.wacc = wacc
+        self.optimization_solver = optimization_solver
+        self.use_scenario_generation = use_scenario_generation
+        self.use_fixed_sample = use_fixed_sample
+        self.use_emission_cap = use_emission_cap
+        self.print_in_iamc_format = print_in_iamc_format
+        self.write_in_lp_format = write_in_lp_format
+        self.serialize_instance = serialize_instance
+        self.north_sea = north_sea
+        
+        # Fixed configuration
+        self.n_reg_season = 4
+        self.regular_seasons = ["winter", "spring", "summer", "fall"]
+        self.n_peak_seasons = 2
+        self.len_peak_season = 24
+        self.leap_years_investment = 5
+        self.time_format = "%d/%m/%Y %H:%M"
+        
+        # Validate the configuration
+        self.validate()
+
+    def validate(self):
+        """
+        Validates the configuration. Raises an error if the configuration is invalid.
+        """
+        pass
+
+    @classmethod
+    def from_dict(cls, config: Dict) -> "EmpireConfiguration":
+        """
+        Constructs EmpireConfiguration object from a dictionary.
+
+        :param config: Dictionary with configurations.
+        :returns: An instance of EmpireConfiguration.
+        """
+        return cls(**config)
+    
+
+class EmpireRunConfiguration:
+    def __init__(
+            self,
+            empire_config: EmpireConfiguration,
+            dataset_path: Path | str,
+            results_path: Path | str
+        ):
+        """
+        Class containing configurations for running Empire simulations.
+
+        :param dataset_path: Folder containing the dataset.
+        :param results_path: Folder where the results should reside.
+        """
+        
+        name = (
+            f"{dataset_path.name}_reg{empire_config.length_of_regular_season}" 
+            +f"_peak{empire_config.len_peak_season}_sce{empire_config.number_of_scenarios}"
+        )
+        
+        if empire_config.use_scenario_generation and not empire_config.use_fixed_sample:
+            name = name + "_randomSGR"
+        else:
+            name = name + "_noSGR"
+        self.name = name + str(datetime.now().strftime("_%Y%m%d%H%M"))
+
+        self.dataset_path = dataset_path
+        self.tab_file_path = dataset_path / f"Tab_Files_{name}"
+        self.scenario_data_path = dataset_path / "ScenarioData"
+        self.results_path = results_path / self.name
+        
+        # Validate the configuration
+        self.validate()
+
+    def validate(self):
+        """
+        Validates the configuration. Raises an error if the configuration is invalid.
+        """
+        pass
+
+    @classmethod
+    def from_dict(cls, config: Dict) -> "EmpireRunConfiguration":
+        """
+        Constructs EmpireRunConfiguration object from a dictionary.
+
+        :param config: Dictionary with configurations.
+        :returns: An instance of EmpireRunConfiguration.
+        """
+        return cls(**config)
---
+++ model_runner.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+import pprint
+from pathlib import Path
+
+from Empire import run_empire
+from Empire.config import EmpireConfiguration, EmpireRunConfiguration, read_config_file
+from Empire.reader import generate_tab_files
+from Empire.scenario_random import generate_random_scenario
+
+
+def run_empire_model(
+        empire_config: EmpireConfiguration,
+        run_config: EmpireRunConfiguration,
+    ):
+
+    horizon = empire_config.forecast_horizon_year
+    NoOfScenarios = empire_config.number_of_scenarios
+    len_reg_season = empire_config.length_of_regular_season
+    discountrate = empire_config.discount_rate
+    use_scen_generation = empire_config.use_scenario_generation
+    use_fix_sample = empire_config.use_fixed_sample
+
+
+    #############################
+    ##Non configurable settings##
+    #############################
+
+    NoOfRegSeason = empire_config.n_reg_season
+    regular_seasons = empire_config.regular_seasons
+    NoOfPeakSeason = empire_config.n_peak_seasons
+    len_peak_season = empire_config.len_peak_season
+    discountrate = empire_config.discount_rate
+    LeapYearsInvestment = empire_config.leap_years_investment
+    time_format = empire_config.time_format
+    north_sea = empire_config.north_sea
+    
+    workbook_path = run_config.dataset_path
+    tab_file_path = run_config.tab_file_path
+    scenario_data_path = run_config.scenario_data_path
+    result_file_path = run_config.results_path
+
+    #######
+    ##RUN##
+    #######
+
+    FirstHoursOfRegSeason = [len_reg_season*i + 1 for i in range(NoOfRegSeason)]
+    FirstHoursOfPeakSeason = [len_reg_season*NoOfRegSeason + len_peak_season*i + 1 for i in range(NoOfPeakSeason)]
+    Period = [i + 1 for i in range(int((horizon-2020)/LeapYearsInvestment))]
+    Scenario = ["scenario"+str(i + 1) for i in range(NoOfScenarios)]
+    peak_seasons = ['peak'+str(i + 1) for i in range(NoOfPeakSeason)]
+    Season = regular_seasons + peak_seasons
+    Operationalhour = [i + 1 for i in range(FirstHoursOfPeakSeason[-1] + len_peak_season - 1)]
+    HoursOfRegSeason = [(s,h) for s in regular_seasons for h in Operationalhour \
+                    if h in list(range(regular_seasons.index(s)*len_reg_season+1,
+                                regular_seasons.index(s)*len_reg_season+len_reg_season+1))]
+    HoursOfPeakSeason = [(s,h) for s in peak_seasons for h in Operationalhour \
+                        if h in list(range(len_reg_season*len(regular_seasons)+ \
+                                            peak_seasons.index(s)*len_peak_season+1,
+                                            len_reg_season*len(regular_seasons)+ \
+                                                peak_seasons.index(s)*len_peak_season+ \
+                                                    len_peak_season+1))]
+    HoursOfSeason = HoursOfRegSeason + HoursOfPeakSeason
+    dict_countries = {"AT": "Austria", "BA": "BosniaH", "BE": "Belgium",
+                    "BG": "Bulgaria", "CH": "Switzerland", "CZ": "CzechR",
+                    "DE": "Germany", "DK": "Denmark", "EE": "Estonia",
+                    "ES": "Spain", "FI": "Finland", "FR": "France",
+                    "GB": "GreatBrit.", "GR": "Greece", "HR": "Croatia",
+                    "HU": "Hungary", "IE": "Ireland", "IT": "Italy",
+                    "LT": "Lithuania", "LU": "Luxemb.", "LV": "Latvia",
+                    "MK": "Macedonia", "NL": "Netherlands", "NO": "Norway",
+                    "PL": "Poland", "PT": "Portugal", "RO": "Romania",
+                    "RS": "Serbia", "SE": "Sweden", "SI": "Slovenia",
+                    "SK": "Slovakia", "MF": "MorayFirth", "FF": "FirthofForth",
+                    "DB": "DoggerBank", "HS": "Hornsea", "OD": "OuterDowsing",
+                    "NF": "Norfolk", "EA": "EastAnglia", "BS": "Borssele",
+                    "HK": "HollandseeKust", "HB": "HelgolanderBucht", "NS": "Nordsoen",
+                    "UN": "UtsiraNord", "SN1": "SorligeNordsjoI", "SN2": "SorligeNordsjoII"}
+
+    print('++++++++')
+    print('+EMPIRE+')
+    print('++++++++')
+    print('Solver: ' + empire_config.optimization_solver)
+    print('Scenario Generation: ' + str(use_scen_generation))
+    print('++++++++')
+    print('ID: ' + run_config.name)
+    print('++++++++')
+
+    if use_scen_generation:
+        generate_random_scenario(
+            file_path = scenario_data_path,
+            tab_file_path = tab_file_path,
+            scenarios = NoOfScenarios,
+            seasons = regular_seasons,
+            Periods = len(Period),
+            regularSeasonHours = len_reg_season,
+            peakSeasonHours = len_peak_season,
+            dict_countries = dict_countries,
+            time_format = time_format,
+            fix_sample = use_fix_sample,
+            north_sea = north_sea
+        )
+
+    generate_tab_files(file_path = workbook_path, tab_file_path = tab_file_path)
+
+    run_empire(name = run_config.name, 
+            tab_file_path = tab_file_path,
+            result_file_path = result_file_path, 
+            scenariogeneration = use_scen_generation,
+            scenario_data_path = scenario_data_path,
+            solver = empire_config.optimization_solver,
+            temp_dir = empire_config.temporary_directory, 
+            FirstHoursOfRegSeason = FirstHoursOfRegSeason, 
+            FirstHoursOfPeakSeason = FirstHoursOfPeakSeason, 
+            lengthRegSeason = len_reg_season,
+            lengthPeakSeason = len_peak_season,
+            Period = Period, 
+            Operationalhour = Operationalhour,
+            Scenario = Scenario,
+            Season = Season,
+            HoursOfSeason = HoursOfSeason,
+            discountrate = discountrate, 
+            WACC = empire_config.wacc, 
+            LeapYearsInvestment = LeapYearsInvestment,
+            IAMC_PRINT = empire_config.print_in_iamc_format, 
+            WRITE_LP = empire_config.write_in_lp_format, 
+            PICKLE_INSTANCE = empire_config.serialize_instance, 
+            EMISSION_CAP = empire_config.use_emission_cap,
+            USE_TEMP_DIR = empire_config.use_temporary_directory
+    )
+
+    with open(run_config.results_path/"config.txt", "w", encoding="utf-8") as file:
+        pprint.pprint(empire_config.__dict__, stream=file, indent=4, width=80)
+
+
+if __name__ == "__main__":
+
+    version = "test"
+
+    ## Read config ##
+    if version == "test":
+        config = read_config_file(Path("config/testmyrun.yaml"))
+    else:
+        config = read_config_file(Path("config/myrun.yaml"))
+
+    empire_config = EmpireConfiguration.from_dict(config=config)
+
+    run_config = EmpireRunConfiguration(
+        empire_config=empire_config,
+        dataset_path=Path.cwd() / f"Data handler/{version}",
+        results_path=Path.cwd() / "Results",
+    )
+
+    run_empire_model(empire_config=empire_config, run_config=run_config)
