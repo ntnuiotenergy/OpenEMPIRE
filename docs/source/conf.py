ca2034dbb1a41b1e35cdb0da5c79a62ec9d4da33
= <martin.hjelmeland@ntnu.no>
2023-10-06 22:53:01 +0200

    Formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ extensions = ["sphinx.ext.autodoc"]
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+pygments_style = 'sphinx'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
