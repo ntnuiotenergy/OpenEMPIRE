6eb5fca831311eb083dc9bf4f672c4e9dc11f192
= <martin.hjelmeland@ntnu.no>
2023-10-06 20:39:00 +0200

    Update config description

--- Empire/config.py
+++ Empire/config.py
@@ -33,21 +33,21 @@ class EmpireConfiguration:
         """
         Class containing configurations for running Empire simulations.
 
-        :param use_temporary_directory: Specifies whether to use a temporary directory for operations
-        :param temporary_directory: Path to the temporary directory used for certain operations
-        :param forecast_horizon_year: Target or end year for a simulation or forecast
-        :param number_of_scenarios: Number of scenarios to consider in the simulation or modeling
-        :param length_of_regular_season: Length of a regulatory or regular season (e.g., in hours)
-        :param discount_rate: Rate used to discount future cash flows to present value
-        :param wacc: Weighted Average Cost of Capital (WACC)
-        :param optimization_solver: Mathematical solver used for optimization tasks
-        :param use_scenario_generation: Indicates if scenarios should be generated dynamically
-        :param use_fixed_sample: Specifies if a certain sample or set of data is fixed
-        :param use_emission_cap: Indicates if there's a cap on emissions
-        :param print_in_iamc_format: Output should be printed in a specific format (e.g., IAMC-compatible)
-        :param write_in_lp_format: Problem should be written in Linear Programming format
-        :param serialize_instance: Serialize the data structure or model for later use
-        :param use_north_sea: Whether north-sea is modelled or not
+        :param use_temporary_directory: Specifies whether to use a temporary directory for operations.
+        :param temporary_directory: Path to the temporary directory used for certain operations.
+        :param forecast_horizon_year: The last strategic (investment) period used in the optimization run. NB! Must correspond with data for version.
+        :param number_of_scenarios: The number of scenarios in every investment period.
+        :param length_of_regular_season: The number of chronological time steps in a regular season. NB! Must correspond with data for version.
+        :param discount_rate: Rate used to discount future cash flows to present value.
+        :param wacc: The Weighted Average Cost of Capital (WACC).
+        :param optimization_solver: Mathematical solver used for optimization tasks. Options: “Xpress”, “Gurobi”, “CPLEX”.
+        :param use_scenario_generation: If true, new operational scenarios will be generated. NB! If false, .tab-files or sampling key must be manually added to the ‘ScenarioData’-folder in the version.
+        :param use_fixed_sample: If true, operational scenarios will be generated according to a fixed sampling key located in the ‘Scenario Data’ folder to ensure the same operational scenarios are generated.
+        :param use_emission_cap: If true, emissions in every scenario are capped according to the specified cap in ‘General.xlsx’. If false, the CO2-price specified in ‘General.xlsx’ applies.
+        :param print_in_iamc_format: OIf true, selected results are printed on the standard IAMC-format in addition to the normal EMPIRE print.
+        :param write_in_lp_format: Problem should be written in Linear Programming format.
+        :param serialize_instance: Serialize the data structure or model for later use.
+        :param use_north_sea: Whether north-sea is modelled or not.
         """
         # Model parameters
         self.use_temporary_directory = use_temporary_directory
