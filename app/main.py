39ae779558a11cfe7b70a9a9d6dc297f678959f6
= <martin.hjelmeland@ntnu.no>
2023-10-27 10:28:10 +0200

    Update streamlit app

--- app/main.py
+++ app/main.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from pathlib import Path
+
+from app.modules.utils import get_active_results
+
+from app.modules.input import input
+from app.modules.output import output
 
 st.set_page_config(layout="wide")
 
@@ -8,6 +14,34 @@ st.markdown(
     """
 Use this app to analyze results from Empire runs. 
 
-In the input page you can analyze the inputs to different model runs, and in the output page you can analyze the results.
+Select the input button to see inputs from the available model runs. Select the output button to see the results.
+
+The app searches for results from the working directory, but you can also provide an absolute path in the sidebar to results located elsewhere.
 """
 )
+
+result_folder = Path.cwd() / "Results"
+other_results = st.sidebar.text_input("Absolute path to other folder with results:")
+
+if other_results:
+    other_results = Path(other_results)
+    if not other_results.exists():
+        raise ValueError("Cannot find the results folder specified")
+
+    active_results = get_active_results([result_folder, other_results])
+
+else:
+    active_results = get_active_results([result_folder])
+
+
+
+if 'current_page' not in st.session_state:
+    st.session_state['current_page'] = 'Input'
+
+with st.sidebar:
+    st.session_state['current_page'] = st.radio("Navigate to", ['Input', 'Output'])
+
+if st.session_state['current_page'] == 'Input':
+    input(active_results)
+elif st.session_state['current_page'] == 'Output':
+    output(active_results)
--- app/modules/utils.py
+++ app/modules/utils.py
@@ -1,7 +1,9 @@
 from pathlib import Path
+
 import streamlit as st
 
-def has_valid_data(folder_path: Path):
+
+def has_valid_data(folder_path: Path) -> bool:
     """
     Check that there is data to illustrate in the results
 
@@ -13,29 +15,37 @@ def has_valid_data(folder_path: Path):
         return False
     results_file = folder_path / "Output/results_objective.csv"
     return results_file.exists()
-    
-def get_valid_data_folders(root_dir: Path):
+
+
+def get_valid_data_folders(folders: list[Path]) -> dict[str, Path]:
     """
     Return folders containig valid data for Empire runs.
 
-    :param root_dir: Directory to search for valid data.
+    :param folders: List of folders to search for valid data.
     """
-    result_folders = []
-    for folder in root_dir.rglob('*'):
-        if folder.is_dir() and has_valid_data(folder):
-            result_folders.append(folder)
 
-    return result_folders
+    valid_result_folders_dict = {}
+    for folder in folders:
+        for f in folder.rglob("*"):
+            if f.is_dir() and has_valid_data(f):
+                relative_path = f.relative_to(folder)
+                if relative_path in valid_result_folders_dict:
+                    raise ValueError(f"Warning relative path name already exists in other result folder. {relative_path}")
+                valid_result_folders_dict[relative_path] = f
+
+    return valid_result_folders_dict
 
 
-def get_active_results(result_folder: Path):
-    
-    valid_result_folders = get_valid_data_folders(result_folder)
+def get_active_results(folders: list[Path]) -> Path:
+    """
+    Create streamlit sidebar with valid results and return path to the active results.
 
-    valid_result_folders_dict = {i.relative_to(result_folder): i for i in valid_result_folders}
+    :param folders: List of folders with results
+    :return: Path to active results
+    """
+
+    valid_result_folders_dict = get_valid_data_folders(folders)
 
     ### Get path to results folder
-    results_folder_relative = st.sidebar.selectbox(
-        "Choose results: ", list(valid_result_folders_dict.keys())
-    )
+    results_folder_relative = st.sidebar.selectbox("Choose results: ", list(valid_result_folders_dict.keys()))
     return valid_result_folders_dict[results_folder_relative]
