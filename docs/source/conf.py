a53c193fc48a1d82e2015fd5b493f3c30ca6d7ee
= <martin.hjelmeland@ntnu.no>
2024-01-24 20:17:33 +0100

    Bump to version 1.0.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@
 project = "OpenEMPIRE"
 copyright = "2023, Stian Backe"
 author = "Stian Backe"
-release = "0.2.0"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
